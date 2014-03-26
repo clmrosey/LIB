d659e4abcd55a5f68d6c2d3ea28aaf62a9db1a74
rosey_c <rosey_c@epitech.eu>
2014-03-26 17:21:51 +0100

    lib

---
+++ my/my_strcat.c
@@ -0,0 +1,21 @@
+/*
+** my_strcat.c for my_strcat in /home/rosey_c/rendu/Piscine-C-Jour_07/ex_01
+**
+** Made by rosey
+** Login   <rosey_c@epitech.net>
+**
+** Started on  Tue Oct  8 11:52:02 2013 rosey
+** Last update Wed Mar 26 11:21:48 2014 rosey
+*/
+
+char	*my_strcat(char *dest, char *src)
+{
+ int	i;
+
+    while (i == 1)
+      {
+	my_putchar(src[1]);
+	my_putchar(dest[1]);
+	i = i + 1;
+      }
+}
